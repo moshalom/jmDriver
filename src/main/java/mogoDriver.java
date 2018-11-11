cd6216373bd476b7a8c3b10176e8c2adb64ee867
moshalom <shalom.moshe1@gmail.com>
2018-11-11 17:10:37 +0200

    fix mongo driver add getCollecitionNames add query function

--- src/main/java/mogoDriver.java
+++ src/main/java/mogoDriver.java
@@ -1,4 +1,9 @@
 import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+
+import javax.swing.text.Document;
 import java.util.List;
 import java.util.Set;
 
@@ -6,20 +11,21 @@ public class mogoDriver {
 
 
     private static MongoClient mongoClient;
-    private static DB database;
+    private static MongoDatabase database;
 
     public static void main (String [] args)
     {
         try{
             mongoClient = new MongoClient("localhost",27017);
-            database = mongoClient.getDB("shalom");
+            database = mongoClient.getDatabase("shalom");
             //DBCollection dbc =  database.getCollection("shalom");
-            //System.out.println("---------------");
+            System.out.println("---------------");
             //System.out.println(dbc.find());
             //System.out.println("---------------");
 
             //getDBNames();
             getDBCollections();
+            collectionQuery("shalom");
 
         }
         catch (Exception ex)
@@ -36,17 +42,31 @@ public class mogoDriver {
          for(String db : dbs){
              System.out.println(db);
          }
+
          return dbs;
      }
 
      private static void getDBCollections()
      {
-         Set<String> tables = database.getCollectionNames();
+         MongoIterable<String> tables = database.listCollectionNames();
 
-        // System.out.println(database.getName());
+        System.out.println("Get collection names for dataBase: " + database.getName());
          for(String coll : tables){
-             System.out.println(coll);
+             System.out.println("111" + coll);
          }
+
+     }
+
+     private static void collectionQuery(String collName)
+     {
+
+         MongoCollection collection =  database.getCollection(collName);
+         BasicDBObject whereQuery = new BasicDBObject();
+         whereQuery.put("name", "ssdds");
+         //DBCursor cursor = collection.find(whereQuery);
+         //while(cursor.hasNext()) {
+         //    System.out.println(cursor.next());
+        // }
      }
 
 
